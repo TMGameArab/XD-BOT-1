92124f4dcda324f1b2b21cd4b1b23d1b32269c7e
ahmedneem23 <40441103+ahmedneem23@users.noreply.github.com>
2018-06-21 10:18:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -852,26 +852,33 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-  if (true) {
-if (message.content === 'رابط') {
-      message.author.send(' |https://discord.gg/QUf4W9k| رابط السرفر').catch(e => console.log(e.stack));
+    if (message.content.startsWith("رابط")) {
 
-    }
-   } 
-  });
-  
-  
-
-client.on('message', message => {
-     if (message.content === "رابط") {
-     let embed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor("#9B59B6")
-  .addField(" Done | تــــم" , " |  تــــم ارســالك في الخــاص")
-     
-     
-     
-  message.channel.sendEmbed(embed);
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 100,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+                .setAuthor(message.guild.name, message.guild.iconURL)
+        .setDescription(`
+**
+---------------------
+-[${message.guild.name}]  هذا هو رابط سيرفر
+---------------------
+-هذا الرابط صالح ل 100 مستخدم فقط
+---------------------
+-هذا الرابط صالح لمده 24 ساعه فقط
+---------------------
+**`)
+      message.author.sendEmbed(Embed11)
     }
 });
 
